4c0e9912d648d5a35d92fcda88ff0cd53aaf25b6
goosearov <103931130+goosearov@users.noreply.github.com>
2023-09-20 21:30:54 +0300

    feature: investingPro translation (#26)

--- src/pages/ui/investingPro/screens/InvestingProTrust/InvestingProTrust.tsx
+++ src/pages/ui/investingPro/screens/InvestingProTrust/InvestingProTrust.tsx
@@ -1,4 +1,6 @@
+import {useTranslation} from "react-i18next";
 import { InvestingProTrustCard } from 'pages/ui/investingPro/screens/InvestingProTrust/InvestingProTrustCard'
+
 import {
   InvestingProTrustImg1,
   InvestingProTrustImg2,
@@ -7,51 +9,55 @@ import {
   InvestingProTrustImg5,
   InvestingProTrustImg6,
 } from 'assets/images'
-
 import s from './InvestingProTrust.module.scss'
 
-const TrustCardData = [
-  {
-    image: InvestingProTrustImg1,
-    title: 'Соблюдаем MiFID II',
-    text: 'Неукоснительно следуем европейскому регуляторному режиму с наивысшим уровнем защиты инвесторов в мире.',
-  },
-  {
-    image: InvestingProTrustImg2,
-    title: 'Европейское регулирование',
-    text: 'Лицензия CySEC и паспортизация во всех странах Евросоюза.Также подотчетны SEC и BaFin.',
-  },
-  {
-    image: InvestingProTrustImg3,
-    title: 'Член AFME',
-    text: 'В составе Европейского ассоциации по финансовым рынкам мы участвуем в разработке инициатив в области регуляции европейских рынков капитала.',
-  },
-  {
-    image: InvestingProTrustImg4,
-    title: 'Надежность доказана аудитами',
-    text: 'Мы проходим регулярные аудиты и процедуры due diligence, которые проводят KPMG, авторизованный SEC консорциум BDOб также S&P и Euroclear.',
-  },
-  {
-    image: InvestingProTrustImg5,
-    title: 'Гарантии Investor Compensation Fund',
-    text: 'Ваши денежные средства находятся под защитой европейского законодательства и застрахованны на сумму до 20 000 евро.',
-  },
-  {
-    image: InvestingProTrustImg6,
-    title: 'Рейтинг S&P на уровне “B/B”',
-    text: 'Международное рейтинговое агенство Standard & Poor’s Global Ratings присвоило нам долгосрочный кредитный рейтинг на уровне “B/B” .',
-  },
-]
-export const InvestingProTrust = () => (
-  <div className={s.trustContainer}>
-    <div className={s.trust_title}>
-      <h1 className={s.main_title}>Почему нам доверяют</h1>
-      <h2 className={s.title_text}>Наши успехи и репутация</h2>
+
+export const InvestingProTrust = () => {
+  const [t] = useTranslation('investingProPage')
+
+  const TrustCardData = [
+    {
+      image: InvestingProTrustImg1,
+      title: t('trust.card1.title'),
+      text: t('trust.card1.label'),
+    },
+    {
+      image: InvestingProTrustImg2,
+      title: t('trust.card2.title'),
+      text: t('trust.card2.label'),
+    },
+    {
+      image: InvestingProTrustImg3,
+      title: t('trust.card3.title'),
+      text: t('trust.card3.label'),
+    },
+    {
+      image: InvestingProTrustImg4,
+      title: t('trust.card4.title'),
+      text: t('trust.card4.label'),
+    },
+    {
+      image: InvestingProTrustImg5,
+      title: t('trust.card5.title'),
+      text: t('trust.card5.label'),
+    },
+    {
+      image: InvestingProTrustImg6,
+      title: t('trust.card6.title'),
+      text: t('trust.card6.label'),
+    },
+  ]
+  return (
+    <div className={s.trustContainer}>
+      <div className={s.trust_title}>
+        <h1 className={s.main_title}>{t('trust.title')}</h1>
+        <h2 className={s.title_text}>{t('trust.label')}</h2>
+      </div>
+      <div className={s.cards}>
+        {TrustCardData.map((card) => (
+          <InvestingProTrustCard card={card} key={card.title} />
+        ))}
+      </div>
     </div>
-    <div className={s.cards}>
-      {TrustCardData.map((card) => (
-        <InvestingProTrustCard card={card} key={card.title} />
-      ))}
-    </div>
-  </div>
-)
+  )
+}
