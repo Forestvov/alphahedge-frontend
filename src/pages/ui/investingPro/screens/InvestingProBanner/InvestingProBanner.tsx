32fdb3a57cdad4f50fe9409520358b13654674f8
goosearov <goosearov@gmail.com>
2023-09-20 21:28:08 +0300

    feature: investingPro translation

--- src/pages/ui/investingPro/screens/InvestingProBanner/InvestingProBanner.tsx
+++ src/pages/ui/investingPro/screens/InvestingProBanner/InvestingProBanner.tsx
@@ -1,40 +1,40 @@
+import { useTranslation } from 'react-i18next'
 import { Container } from 'components/shared/Container'
 import { LightningIcon } from 'assets/icons'
-import { InvestingProBannerImg } from 'assets/images'
 
+import { InvestingProBannerImg } from 'assets/images'
 import s from './InvestingProBanner.module.scss'
 
-export const InvestingProBanner = () => (
-  <div className={s.banner}>
-    <Container>
-      <div className={s.content}>
-        <h1 className={s.title}>
-          Фондовый рынок – это <span>движение</span> ,<br /> это{' '}
-          <span>эволюция</span>, это
-          <span>искусство</span> прогнозирования.
-        </h1>
-        <ul className={s.list}>
-          <li className={s.item}>
-            <img src={LightningIcon} alt="rhombus" />
-            Мы придерживаемся подхода, который объединяет надежность и
-            инновации, стремясь обеспечить стабильный рост в долгосрочной
-            перспективе.
-          </li>
-          <li className={s.item}>
-            <img src={LightningIcon} alt="rhombus" />
-            Наши эксперты постоянно отслеживают тенденции рынка и проводят
-            фундаментальный анализ, чтобы определить потенциально
-            привлекательные возможности.
-          </li>
-          <li className={s.item}>
-            <img src={LightningIcon} alt="rhombus" />
-            Наша миссия заключается в создании и управлении портфелем активов,
-            который соответствует нашим целям и философии, а также способствует
-            достижению ваших инвестиционных амбиций.
-          </li>
-        </ul>
-      </div>
-    </Container>
-    <img className={s.image} src={InvestingProBannerImg} alt="banner" />
-  </div>
-)
+export const InvestingProBanner = () => {
+  const [t] = useTranslation('investingProPage')
+
+  return (
+    <div className={s.banner}>
+      <Container>
+        <div className={s.content}>
+          <h1 className={s.title}>
+            {t('banner.title')} <span>{t('banner.titlePink')}</span> ,<br />{' '}
+            {t('banner.title1')} <span>{t('banner.titlePink1')}</span>,{' '}
+            {t('banner.title2')}
+            <span> {t('banner.titlePink2')}</span> {t('banner.title3')}
+          </h1>
+          <ul className={s.list}>
+            <li className={s.item}>
+              <img src={LightningIcon} alt="rhombus" />
+              {t('banner.text1')}
+            </li>
+            <li className={s.item}>
+              <img src={LightningIcon} alt="rhombus" />
+              {t('banner.text2')}
+            </li>
+            <li className={s.item}>
+              <img src={LightningIcon} alt="rhombus" />
+              {t('banner.text3')}
+            </li>
+          </ul>
+        </div>
+      </Container>
+      <img className={s.image} src={InvestingProBannerImg} alt="banner" />
+    </div>
+  )
+}
