913295c20c8a3200e782d5a02409af29fdccde49
goosearov <goosearov@gmail.com>
2023-09-21 11:29:22 +0300

    feature: contactsPage translation

--- src/pages/ui/contacts/Contacts.tsx
+++ src/pages/ui/contacts/Contacts.tsx
@@ -1,56 +1,58 @@
+import { useTranslation } from 'react-i18next'
 import { Container } from 'components/shared/Container'
 import { Mapimg } from 'assets/images'
 import { ContactsCard } from './ContactsCard'
 import s from './Contacts.module.scss'
 
-const ContactsCards = [
-  {
-    title: 'Адресс',
-    text: 'Главный офис расположен в ',
-    link: 'Travessera de Gràcia, 21, 08021 Барселона, Испания',
-    type: 'text',
-  },
-  {
-    title: 'Почта',
-    text: 'Вопросы или проблемы? Свяжитесь с нами!',
-    link: 'ah-support@gmail.com',
-    type: 'mail',
-  },
-  {
-    title: 'Telegram новости',
-    text: 'Получайте новости о мире финансов!',
-    link: 'https://t.me/alphahedge_holding',
-    type: 'link',
-  },
-  {
-    title: 'Telegram форекс',
-    text: 'Получайте аналитку форекс!',
-    link: 'https://t.me/alphahedge_forex',
-    type: 'link',
-  },
-]
+export const Contacts = () => {
+  const [t] = useTranslation('contactsPage')
 
-export const Contacts = () => (
-  <Container>
-    <div className={s.content}>
-      <div className={s.left}>
-        <span className={s.label}>Контакты</span>
-        <h1 className={s.title}>Мы профессиональная команда</h1>
-        <p className={s.text}>
-          Свяжитесь с Нами для Обсуждения Инвестиционных Возможностей
-        </p>
-        <img className={s.image} src={Mapimg} alt="Map" />
+  const ContactsCards = [
+    {
+      title: t('card1.title'),
+      text: t('card1.text'),
+      link: `Travessera de Gràcia, 21, 08021 ${t('card1.link')}`,
+      type: 'text',
+    },
+    {
+      title: t('card2.title'),
+      text: t('card2.text'),
+      link: 'ah-support@gmail.com',
+      type: 'mail',
+    },
+    {
+      title: t('card3.title'),
+      text: t('card3.text'),
+      link: 'https://t.me/alphahedge_holding',
+      type: 'link',
+    },
+    {
+      title: t('card4.title'),
+      text: t('card4.text'),
+      link: 'https://t.me/alphahedge_forex',
+      type: 'link',
+    },
+  ]
+  return (
+    <Container>
+      <div className={s.content}>
+        <div className={s.left}>
+          <span className={s.label}>{t('label')}</span>
+          <h1 className={s.title}>{t('title')}</h1>
+          <p className={s.text}>{t('text')}</p>
+          <img className={s.image} src={Mapimg} alt="Map" />
+        </div>
+        <div className={s.right}>
+          {ContactsCards.map((card) => (
+            <ContactsCard
+              title={card.title}
+              text={card.text}
+              link={card.link}
+              type={card.type}
+            />
+          ))}
+        </div>
       </div>
-      <div className={s.right}>
-        {ContactsCards.map((card) => (
-          <ContactsCard
-            title={card.title}
-            text={card.text}
-            link={card.link}
-            type={card.type}
-          />
-        ))}
-      </div>
-    </div>
-  </Container>
-)
+    </Container>
+  )
+}
