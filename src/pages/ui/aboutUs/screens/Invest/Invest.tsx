ab36712a404f9e8f4227828ac8228a6f0d9d08fa
goosearov <goosearov@gmail.com>
2023-09-21 20:30:51 +0300

    feature: aboutUsPage translation

--- src/pages/ui/aboutUs/screens/Invest/Invest.tsx
+++ src/pages/ui/aboutUs/screens/Invest/Invest.tsx
@@ -1,44 +1,43 @@
+import { useTranslation } from 'react-i18next'
 import { Container } from 'components/shared/Container'
 import { AboutInvestImg } from 'assets/images'
 import { LightningIcon } from 'assets/icons'
 import s from './Invest.module.scss'
 
-export const Invest = () => (
-  <section className={s.invest}>
-    <Container>
-      <h2 className={s.title}>
-        <span>Долгосрочное инвестирование</span> и инновации в фокусе
-      </h2>
-      <div className={s.content}>
-        <div className={s.desc}>
-          <p className={s.text}>
-            Фонд Alphahedge Holdings пропагандировал принцип долгосрочного
-            инвестирования, основанного на фундаментальном анализе компаний и
-            рынков. Одной из ключевых особенностей фонда был акцент на выборе
-            компаний, которые демонстрировали инновационный подход к бизнесу и
-            были ориентированы на будущее.
-          </p>
-          <div className={s.bottom}>
-            <div className={s.item}>
-              <img src={LightningIcon} alt="lightning" />
-              Долгосрочное инвестирование
-            </div>
-            <div className={s.item}>
-              <img src={LightningIcon} alt="lightning" />
-              Фундаментальный анализ
-            </div>
-            <div className={s.item}>
-              <img src={LightningIcon} alt="lightning" />
-              Инновационный подход
-            </div>
-            <div className={s.item}>
-              <img src={LightningIcon} alt="lightning" />
-              Ориентация на будущее
+export const Invest = () => {
+  const [t] = useTranslation('aboutUsPage')
+
+  return (
+    <section className={s.invest}>
+      <Container>
+        <h2 className={s.title}>
+          <span>{t('invest.titlePink')}</span> {t('invest.title')}
+        </h2>
+        <div className={s.content}>
+          <div className={s.desc}>
+            <p className={s.text}>{t('invest.text')}</p>
+            <div className={s.bottom}>
+              <div className={s.item}>
+                <img src={LightningIcon} alt="lightning" />
+                {t('invest.item1')}
+              </div>
+              <div className={s.item}>
+                <img src={LightningIcon} alt="lightning" />
+                {t('invest.item2')}
+              </div>
+              <div className={s.item}>
+                <img src={LightningIcon} alt="lightning" />
+                {t('invest.item3')}
+              </div>
+              <div className={s.item}>
+                <img src={LightningIcon} alt="lightning" />
+                {t('invest.item4')}
+              </div>
             </div>
           </div>
+          <img className={s.image} src={AboutInvestImg} alt="invest" />
         </div>
-        <img className={s.image} src={AboutInvestImg} alt="invest" />
-      </div>
-    </Container>
-  </section>
-)
+      </Container>
+    </section>
+  )
+}
