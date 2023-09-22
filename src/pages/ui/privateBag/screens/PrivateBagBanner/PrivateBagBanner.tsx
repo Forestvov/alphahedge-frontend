53eb53cf73160c983b3660ea045a4c857885274e
goosearov <goosearov@gmail.com>
2023-09-22 11:54:34 +0300

    feature: privateBagPage translation

--- src/pages/ui/privateBag/screens/PrivateBagBanner/PrivateBagBanner.tsx
+++ src/pages/ui/privateBag/screens/PrivateBagBanner/PrivateBagBanner.tsx
@@ -1,50 +1,48 @@
+import { useTranslation } from 'react-i18next'
 import { Container } from 'components/shared/Container'
 import { RhombusIcon } from 'assets/icons'
-import { PrivateBagBannerImg } from 'assets/images'
 
+import { PrivateBagBannerImg } from 'assets/images'
 import s from './PrivateBagBanner.module.scss'
 
-export const PrivateBagBanner = () => (
-  <div className={s.banner}>
-    <Container>
-      <div className={s.content}>
-        <h1 className={s.title}>
-          <span>Частный портфель</span>
-          <br />
-          Удобный и эффективный способ вложения денежных средств
-        </h1>
-        <p className={s.text}>
-          Решение накопительного страхования жизни для накоплений и совершения
-          инвестиций. Удобное решение, если хотите доверить управление
-          инвестициями опытным экспертам.
-        </p>
-        <ul className={s.list}>
-          <li className={s.item}>
-            <img src={RhombusIcon} alt="rhombus" />
-            Инвестиции в соответствии с выбранной стратегией
-          </li>
-          <li className={s.item}>
-            <img src={RhombusIcon} alt="rhombus" />
-            Возможность указать выгодоприобретателя
-          </li>
-          <li className={s.item}>
-            <img src={RhombusIcon} alt="rhombus" />
-            Налог с полученной прибыли рассчитывается и удерживается
-            автоматически
-          </li>
-          <li className={s.item}>
-            <img src={RhombusIcon} alt="rhombus" />
-            Возможность каждый год получать возврат подоходного налога с
-            населения в размере 20%
-          </li>
-          <li className={s.item}>
-            <img src={RhombusIcon} alt="rhombus" />
-            Более выгодная комиссия за администрирование, от 0,29% до 0,99% в
-            год
-          </li>
-        </ul>
-      </div>
-    </Container>
-    <img className={s.image} src={PrivateBagBannerImg} alt="banner" />
-  </div>
-)
+export const PrivateBagBanner = () => {
+  const [t] = useTranslation('privateBagPage')
+
+  return (
+    <div className={s.banner}>
+      <Container>
+        <div className={s.content}>
+          <h1 className={s.title}>
+            <span>{t('banner.titlePink')}</span>
+            <br />
+            {t('banner.title')}
+          </h1>
+          <p className={s.text}>{t('banner.text')}</p>
+          <ul className={s.list}>
+            <li className={s.item}>
+              <img src={RhombusIcon} alt="rhombus" />
+              {t('banner.item1')}
+            </li>
+            <li className={s.item}>
+              <img src={RhombusIcon} alt="rhombus" />
+              {t('banner.item2')}
+            </li>
+            <li className={s.item}>
+              <img src={RhombusIcon} alt="rhombus" />
+              {t('banner.item3')}
+            </li>
+            <li className={s.item}>
+              <img src={RhombusIcon} alt="rhombus" />
+              {t('banner.item4')}
+            </li>
+            <li className={s.item}>
+              <img src={RhombusIcon} alt="rhombus" />
+              {t('banner.item5')}
+            </li>
+          </ul>
+        </div>
+      </Container>
+      <img className={s.image} src={PrivateBagBannerImg} alt="banner" />
+    </div>
+  )
+}
