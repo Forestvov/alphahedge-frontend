e886d9c25253895cba72c02bc4a5ad6eff6ef320
goosearov <103931130+goosearov@users.noreply.github.com>
2023-09-22 12:43:24 +0300

    feature: etfPage translation (#32)

--- src/pages/ui/etfPage/screens/EtfBanner/EtfBanner.tsx
+++ src/pages/ui/etfPage/screens/EtfBanner/EtfBanner.tsx
@@ -1,39 +1,42 @@
+import { useTranslation } from 'react-i18next'
 import { Container } from 'components/shared/Container'
 import { RhombusIcon } from 'assets/icons'
-import { ETfBannerImg } from 'assets/images'
 
+import { ETfBannerImg } from 'assets/images'
 import s from './EtfBanner.module.scss'
 
-export const EtfBanner = () => (
-  <div className={s.banner}>
-    <Container>
-      <div className={s.content}>
-        <h1 className={s.title}>
-          <span>Биржевые фонды (ETF)</span>
-          <br />
-          Простой способ начать инвестировать
-        </h1>
-        <p className={s.text}>
-          ETF – это фонды вложений, доли (удостоверения вложений) которых
-          торгуются на фондовых биржах. Основные преимущества ETF:
-        </p>
-        <ul className={s.list}>
-          <li className={s.item}>
-            <img src={RhombusIcon} alt="rhombus" />
-            Совершать сделки с ETF так же просто, как и с акциями
-          </li>
-          <li className={s.item}>
-            <img src={RhombusIcon} alt="rhombus" />
-            Возможность быстро и просто самостоятельно сформировать
-            диверсифицированный портфель ценных бумаг
-          </li>
-          <li className={s.item}>
-            <img src={RhombusIcon} alt="rhombus" />
-            Более низкие комиссии, чем при покупке каждой акции в отдельности
-          </li>
-        </ul>
-      </div>
-    </Container>
-    <img className={s.image} src={ETfBannerImg} alt="banner" />
-  </div>
-)
+export const EtfBanner = () => {
+  const [t] = useTranslation('etfPage')
+
+  return (
+    <div className={s.banner}>
+      <Container>
+        <div className={s.content}>
+          <h1 className={s.title}>
+            <span>{t('banner.titlePink')}</span>
+            <br />
+            {t('banner.title')}
+          </h1>
+          <p className={s.text}>
+            {t('banner.text')}
+          </p>
+          <ul className={s.list}>
+            <li className={s.item}>
+              <img src={RhombusIcon} alt="rhombus" />
+              {t('banner.item1')}
+            </li>
+            <li className={s.item}>
+              <img src={RhombusIcon} alt="rhombus" />
+              {t('banner.item2')}
+            </li>
+            <li className={s.item}>
+              <img src={RhombusIcon} alt="rhombus" />
+              {t('banner.item3')}
+            </li>
+          </ul>
+        </div>
+      </Container>
+      <img className={s.image} src={ETfBannerImg} alt="banner" />
+    </div>
+  )
+}
--- src/pages/ui/etfPage/screens/EtfCards/EtfCards.tsx
+++ src/pages/ui/etfPage/screens/EtfCards/EtfCards.tsx
@@ -1,61 +1,62 @@
+import { useTranslation } from 'react-i18next'
 import { Container } from 'components/shared/Container'
 import { PrivateBagBg1, PrivateBagBg2 } from 'assets/images'
 import { EtfCard } from 'pages/ui/etfPage/screens/EtfCards/EtfCard'
 import s from './EtfCards.module.scss'
 
-const EtfCardsData = [
-  {
-    title: 'Диверсификация',
-    text: [
-      'Одним вложением вы приобретаете готовую корзину активов: от акций и облигаций до товаров и валюты.',
-      'В состав ETF входят разные инструменты и активы, позволяя удобно диверсифицировать инвестиционные риски.',
-    ],
-  },
-  {
-    title: 'Экономия времени',
-    text: [
-      'Инвестиции в ETF не требуют специальных знаний или профессиональной квалификации и доступны большинству частных инвесторов.',
-      'Фонды ETF позволяют инвестировать в отрасли или индексы, которые вызывают интерес и доверие, не тратя время на изучение рынка и подбор активов.',
-    ],
-  },
-  {
-    title: 'Ликвидность',
-    text: [
-      'Также как и сделки с акциями, сделки с ETF можно совершать в любое время работы биржи.',
-      'Фонды ETF удобны тем, что их доли можно быстро продать и купить в желаемом объёме по приемлемой для инвестора цене.',
-    ],
-  },
-  {
-    title: 'Потенциальные риски',
-    text: [
-      'Фонды ETF, как и любой другой инвестиционный инструмент, подвержены различного рода рискам, например, законодательному риску, риску валютных колебаний, налоговому риску, риску процентных ставок и др.',
-      'Очень важно понимать эти риски и самостоятельно их оценивать*.',
-    ],
-  },
-]
+export const EtfCards = () => {
+  const [t] = useTranslation('etfPage')
 
-export const EtfCards = () => (
-  <>
-    <div className={s.etf}>
-      <Container>
-        <div className={s.content}>
-          <h2 className={s.title}>Что такое ETF (торгуемые на бирже фонды)?</h2>
-          <p className={s.desc}>
-            ETF – это фонды вложений, которые торгуются на бирже и могут
-            включать разные классы активов, например, акции, облигации, валютные
-            инструменты, сырьевые активы и др.
-          </p>
-          <div className={s.cards}>
-            {EtfCardsData.map((card) => (
-              <EtfCard card={card} key={card.title} />
-            ))}
+  const EtfCardsData = [
+    {
+      title: t('cards.card1.title'),
+      text: [
+        t('cards.card1.desc1'),
+        t('cards.card1.desc2'),
+      ],
+    },
+    {
+      title: t('cards.card2.title'),
+      text: [
+        t('cards.card2.desc1'),
+        t('cards.card2.desc2'),
+      ],
+    },
+    {
+      title: t('cards.card3.title'),
+      text: [
+        t('cards.card3.desc1'),
+        t('cards.card3.desc2'),
+      ],
+    },
+    {
+      title: t('cards.card4.title'),
+      text: [
+        t('cards.card4.desc1'),
+        t('cards.card4.desc2'),
+      ],
+    },
+  ]
+
+  return (
+    <>
+      <div className={s.etf}>
+        <Container>
+          <div className={s.content}>
+            <h2 className={s.title}>{t('cards.title')}</h2>
+            <p className={s.desc}>{t('cards.text')}</p>
+            <div className={s.cards}>
+              {EtfCardsData.map((card) => (
+                <EtfCard card={card} key={card.title} />
+              ))}
+            </div>
           </div>
-        </div>
-      </Container>
-    </div>
-    <div className={s.bottom}>
-      <img className={s.image1} src={PrivateBagBg1} alt="bg" />
-      <img className={s.image2} src={PrivateBagBg2} alt="bg" />
-    </div>
-  </>
-)
+        </Container>
+      </div>
+      <div className={s.bottom}>
+        <img className={s.image1} src={PrivateBagBg1} alt="bg" />
+        <img className={s.image2} src={PrivateBagBg2} alt="bg" />
+      </div>
+    </>
+  )
+}
